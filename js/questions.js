ac7aeefc6f1062b58473a76f86ee2190a35ade3b
MattVlla <105332915+MattVlla@users.noreply.github.com>
2022-06-14 23:16:08 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What is JavaScript?",
+    answer: "A Scripting Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "A Scripting Language",
+      "A Object-Oriented Language",
+      "A Mark Up Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What are JavaScript Data Types?",
+    answer: "All of the above",
+    options: [
+      "Number",
+      "String",
+      "Objet",
+      "All of the above"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does JavaScript do?",
+    answer: "Makes app Interactive",
+    options: [
+      "Makes app Interactive",
+      "Styles the Webpage",
+      "Adds color",
+      "Creates text"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What is a looping structure?",
+    answer: "For loop",
+    options: [
+      "For loop",
+      "Style loop",
+      "Dynamic loop",
+      "Why loop"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What would be the result of 3+2+'7'?",
+    answer: "57",
+    options: [
+      "12",
+      "5+7",
+      "3+9",
+      "57"
+    ]
+  },
+  
+];
